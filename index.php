75a5657472de5085c244a2bc243ed1f82a8e9d39
narenverma <89765847+narenverma@users.noreply.github.com>
2021-09-08 14:42:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<?php include('assets/includes/metatags.php'); ?>
+	<title></title>
+	<?php include('assets/includes/header-links.php'); ?>
+
+
+</head>
+<body>
+	<?php include('assets/includes/header.php'); ?>
+
+     <main>
+     	
+     </main>
+	<?php include('assets/includes/footer.php'); ?>
+	<?php include('assets/includes/footers-links.php'); ?>
+
+</body>
+</html>
